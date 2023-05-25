b9585372b7fee89c4e5aa7224e026aee7dd6de69
John Bender <john.b@crowdbotics.com>
2023-05-25 16:11:18 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ modules/blackbaud/options.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({});
+
+export default {
+  styles: styles,
+  issuer: "https://oauth2.sky.blackbaud.com/",
+  clientId: "Your blackbaud clientId",
+  redirectUrl: "com.demo:/callback",
+  successNavScreen: "blackbaudEventListing"
+};
--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "my-mobile-app-40999",
-  url: "https://my-mobile-app-40999.botics.co",
-  api: "https://my-mobile-app-40999.botics.co/api/v1"
-}
+  "name": "my-mobile-app-40999",
+  "url": "https://my-mobile-app-40999.botics.co",
+  "api": "https://my-mobile-app-40999.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
